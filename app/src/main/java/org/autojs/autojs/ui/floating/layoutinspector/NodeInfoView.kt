194a2504329738bbb9be2430ba8110ff41be1748
AZAD\azad4ever <azad4ever2nd@outlook.com>
2025-08-30 10:57:25 +0800

    修改nodeview.kt

--- app/src/main/java/org/autojs/autojs/ui/floating/layoutinspector/NodeInfoView.kt
+++ app/src/main/java/org/autojs/autojs/ui/floating/layoutinspector/NodeInfoView.kt
@@ -133,11 +133,11 @@ class NodeInfoView : RecyclerView {
         when (attr) {
             "className" -> value = value.replace("^android\\.widget\\.".toRegex(), "")
             "actionNames" -> return "action(${value.split("\n").joinToString(", ") { "'$it'" }})"
-            "bounds" -> return "$attr(${value.replace("[^\\d,]".toRegex(), "").replace(",", ", ")})"
+            "bounds" -> return "$attr(${value.replace("[^\\d,]".toRegex(), "").replace(",", ",")})"
         }
         return when (NodeInfo::class.java.getDeclaredField(attr).type) {
-            java.lang.String::class.java -> "$attr('$value')"
-            else -> "$attr($value)"
+            java.lang.String::class.java -> "$value"
+            else -> "$value"
         }
     }
 
